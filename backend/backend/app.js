cc0185500b723f22fdaadb1b556cbd215d39cb04
rameshbeast <bistramesh321@gmail.com>
2026-03-21 02:15:51 +0000

    Refactor app.js to use Express and MySQL

--- backend/backend/app.js
+++ backend/backend/app.js
@@ -1,10 +1,52 @@
-const http = require("http");
+const express = require("express");
+const app = express();
+const mysql = require("mysql2");
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Study Buddy Backend Running ✅");
+app.set("view engine", "pug");
+app.set("views", "./backend/views");
+
+const db = mysql.createConnection({
+  host: "db",
+  user: "root",
+  password: "password",
+  database: "studybuddy"
 });
 
-server.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+db.connect();
+
+// USERS
+app.get("/users", (req, res) => {
+  db.query("SELECT * FROM users", (err, result) => {
+    res.render("users", { users: result });
+  });
 });
+
+// PROFILE
+app.get("/profile/:id", (req, res) => {
+  db.query("SELECT * FROM users WHERE id=?", [req.params.id], (err, result) => {
+    res.render("profile", { user: result[0] });
+  });
+});
+
+// SESSIONS
+app.get("/sessions", (req, res) => {
+  db.query("SELECT * FROM sessions", (err, result) => {
+    res.render("sessions", { sessions: result });
+  });
+});
+
+// DETAIL
+app.get("/sessions/:id", (req, res) => {
+  db.query("SELECT * FROM sessions WHERE id=?", [req.params.id], (err, result) => {
+    res.render("detail", { session: result[0] });
+  });
+});
+
+// TAGS
+app.get("/tags", (req, res) => {
+  db.query("SELECT * FROM tags", (err, result) => {
+    res.render("tags", { tags: result });
+  });
+});
+
+app.listen(3000);
